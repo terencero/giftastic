502ead8527332f0e8d7cf2ec0c435aa11fe3e414
Terence Ro <terencero@Terences-MacBook-Air.local>
2016-11-06 16:42:00 -0600

    add parameters, change the on click function to target buttons

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,11 +1,13 @@
 var topics = ['trees', 'flowers', 'bees', 'lakes', 'clouds'];
 
-$('#find-nature').on('click', function(){
+//$('#find-nature').on('click', function(){
+$('.nature-btn').on('click', function(){	
 
-	var nature = $('#nature-input').val();
-
+	var addNature = $('#nature-input').val();
+	var nature = $(this).data('nature');
 	var apiKey = '&api_key=dc6zaTOxFJmzC';
-	var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + nature + apiKey;
+	var param = '&limit=10&rating=g&rating=pg&rating=pg-13';
+	var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + nature + apiKey + param;
 
 	$.ajax({url: queryURL, method: 'GET'}).done(function(response){
 		console.log(response);
